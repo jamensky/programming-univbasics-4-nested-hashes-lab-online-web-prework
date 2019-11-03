0233c747fc31266e0ced3723ef3b23f25cbfc8c8
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-03 20:03:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+end programmer_hash[:dennis_ritchie][:languages]
 
 def adding_matz
 # add the following information to the top level of programmer_hash
